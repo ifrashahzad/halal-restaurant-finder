85bbb927b54c0ea38336ea9c563947c4e647aced
Ifra Shahzad <ifrachattha31@gmail.com>
2026-03-27 21:18:31 +0500

    Updated features

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: 'all',
+    allowedHosts: 'halal-restaurant-finder-production-dea5.up.railway.app',
   },
 })
